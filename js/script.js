d1a0f2a30a037d39a5570d1e92d4ff8d497d04c3
Rifaldo Hi Djafar <sellyfmanuhua@gmail.com>
2023-06-07 18:46:13 +0800

    KedaiQzie-Roastery

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Toogle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+(".navbar-nav");
+
+//ketika kedai-menu di klik
+
+document.querySelector("#kedai-menu").onclick = () => {
+
+  navbarNav.classList.toggle("active");
+
+};
+
+
+
+// Klik di luar sidebar untuk menghilangkan nav
+
+const kedai = document.querySelector("#kedai-menu");
+
+
+
+document.addEventListener("click", function (e) {
+
+  if (!kedai.contains(e.target) && !navbarNav.contains(e.target)) {
+
+    navbarNav.classList.remove("active");
+
+  }
+
+});
+
